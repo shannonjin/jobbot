d4d0fe2bfd24b681c93784162e92e41f7eff2bb5
shannonjin <shannonj112@gmail.com>
2024-05-03 14:19:28 -0400

    Fix scrolling to handle DOM stale

--- webdriver.py
+++ webdriver.py
@@ -1,8 +1,11 @@
 # jobbot
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 import time
-
 
 driver = webdriver.Chrome()
 driver.get("https://linkedin.com")
@@ -19,6 +22,7 @@ password.send_keys("")
 
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
 driver.implicitly_wait(0.5)
+time.sleep(20)
 driver.find_element(By.CSS_SELECTOR, "[href='https://www.linkedin.com/jobs/?']").click()
 driver.implicitly_wait(0.5)
 
@@ -26,16 +30,20 @@ driver.get('https://www.linkedin.com/jobs/search?keywords=software%20engineer&lo
 
 job_block = driver.find_element(By.CLASS_NAME, "scaffold-layout__list-container")
 jobs = job_block.find_elements(By.TAG_NAME, "li")
-f = open("myfile.txt", "w")
+driver.implicitly_wait(3.0)
 
 for job in jobs:
-   # d1 = job.find_element(By.CLASS_NAME, "flex-grow-1 artdeco-entity-lockup__content ember-view")
+   # d1 = job.find_element(By.XPATH, "//div[@data-view-name='job-card']")
     #d2 = d1.find_element(By.CLASS_NAME, "full-width artdeco-entity-lockup__title ember-view")
-   # link = d2.find_element(By.CLASS_NAME, 'disabled ember-view job-card-container__link job-card-list__title job-card-list__title--link')
-    job.click()
-    f.write("Now the file has more content!")
-
-    driver.implicitly_wait(3.0)
+    #link = d2.find_element(By.CLASS_NAME, 'disabled ember-view job-card-container__link job-card-list__title job-card-list__title--link')
+    #link.click()
+    #f.write("Now the file has more content!")
+   #ignored_exceptions = (NoSuchElementException, StaleElementReferenceException,)
+  # your_element = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions) \
+   #   .until(expected_conditions.presence_of_element_located(job))
+   #ActionChains(driver).move_to_element(job).click().perform()
+   ActionChains(driver).double_click(job).perform()
+   driver.implicitly_wait(0.5)
    # applyButton = driver.find_elements(By.XPATH, "//button[@type='submit']")
     #if len(applyButton) > 0:
        # applyButton[0].click()
