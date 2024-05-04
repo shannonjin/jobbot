25f6da1556521aac7816614c70645060a1d06d38
shannonjin <shannonj112@gmail.com>
2024-05-03 23:22:26 -0400

    Opens new tab using Apply button

--- webdriver.py
+++ webdriver.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
@@ -14,11 +16,10 @@ title = driver.title
 
 driver.implicitly_wait(0.5)
 
-username = driver.find_element(By.XPATH,"//input[@name='session_key']")
-password = driver.find_element(By.XPATH,"//input[@name='session_password']")
+username = driver.find_element(By.XPATH, "//input[@name='session_key']")
+password = driver.find_element(By.XPATH, "//input[@name='session_password']")
 username.send_keys("sj2802@columbia.edu")
-password.send_keys("")
-
+password.send_keys("U7MT=22Tp-T%.UY")
 
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
 driver.implicitly_wait(0.5)
@@ -30,25 +31,57 @@ driver.get('https://www.linkedin.com/jobs/search?keywords=software%20engineer&lo
 
 job_block = driver.find_element(By.CLASS_NAME, "scaffold-layout__list-container")
 jobs = job_block.find_elements(By.TAG_NAME, "li")
-driver.implicitly_wait(3.0)
+n = len(jobs)
+f = open("demofile2.txt", "a")
+i = 0
 
-for job in jobs:
-   # d1 = job.find_element(By.XPATH, "//div[@data-view-name='job-card']")
-    #d2 = d1.find_element(By.CLASS_NAME, "full-width artdeco-entity-lockup__title ember-view")
-    #link = d2.find_element(By.CLASS_NAME, 'disabled ember-view job-card-container__link job-card-list__title job-card-list__title--link')
-    #link.click()
-    #f.write("Now the file has more content!")
-   #ignored_exceptions = (NoSuchElementException, StaleElementReferenceException,)
-  # your_element = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions) \
-   #   .until(expected_conditions.presence_of_element_located(job))
-   #ActionChains(driver).move_to_element(job).click().perform()
-   ActionChains(driver).double_click(job).perform()
-   driver.implicitly_wait(0.5)
-   # applyButton = driver.find_elements(By.XPATH, "//button[@type='submit']")
-    #if len(applyButton) > 0:
-       # applyButton[0].click()
+while i < n:
+    driver.implicitly_wait(0.5)
+    jobs = job_block.find_elements(By.TAG_NAME, "li")
+    n = len(jobs)
+    i += 1
+    # f.write(str(len(jobs)) + '\n')
+    if i < n:
+        ActionChains(driver).double_click(jobs[i]).perform()
+        driver.implicitly_wait(0.5)
+        driver.switch_to.window(driver.window_handles[-1])
+        d1 = driver.find_element(By.CLASS_NAME, "jobs-search__job-details--wrapper")
+        d2 = d1.find_element(By.CLASS_NAME, "job-view-layout.jobs-details")
+        d3 = d2.find_element(By.CLASS_NAME, "t-14")
+        d4 = d3.find_element(By.CLASS_NAME, "mt5")
+        d5 = d4.find_element(By.CLASS_NAME, "display-flex")
+        d6 = d5.find_element(By.CLASS_NAME, "jobs-s-apply.jobs-s-apply--fadein.inline-flex.mr2")
+        driver.implicitly_wait(0.5)
+        try:
+            #f.write("trying\n")
+            #d7 = d6.find_element(By.CLASS_NAME, "jobs-apply-button--top-card")
+           # d6.find_element(By.XPATH, "//button").click()
+            #button = d6.find_element(By.CLASS_NAME, "jobs-apply-button.artdeco-button.artdeco-button--icon-right.artdeco-button--3 artdeco-button--primary.ember-view")
+           # button.click()
+            WebDriverWait(driver, 3).until(
+                EC.element_to_be_clickable((By.XPATH, "//span[normalize-space(.)='Apply']"))).click()
+            f.write('button clicked\n')
+            time.sleep(5)
+        except:
+            f.write('continue\n')
+            continue
 
-#jobs_list = jobs_block.find_elements(By.CSS_SELECTOR, ".jobs-search-results__list-item")
+         #find_element(By.CSS_SELECTOR, "[href='#link-external-small']").click())
+
+      #  ActionChains(driver).double_click(d5.find_element(By.XPATH)).perform()
+
+    #wait = WebDriverWait(driver, 100)
+    #element = wait.until(EC.element_to_be_clickable(jobs[i]))
+    # element.click()
+    driver.implicitly_wait(0.5)
+# driver.find_element(By.XPATH, "//button[@text()='Apply']").click()
+#  e2 = wait.until(EC.element_to_be_clickable(By.CLASS_NAME, "scaffold-layout__list-detail-container"))
+
+# applyButton = driver.find_elements(By.XPATH, "//button[@type='submit']")
+#if len(applyButton) > 0:
+# applyButton[0].click()
+
+#jobs_list = jobs_block.find_elements(By.CSS_SELECTOR, ".jobs-search-resultsw__list-item")
 
 time.sleep(10)
 
